b18d6a12163d598759c799766ba3db492bc86cca
juliusmartins <juliusmla@hotmail.com>
2023-05-08 16:29:24 -0300

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -2,7 +2,8 @@
 using namespace std;
 int main()
 {
-int indice=0;
+int indice = 0;
+int marcador_de_variavel = 0;
 string analisador;
 string entrada;
 getline(cin,entrada);
@@ -14,9 +15,15 @@ while(indice<entrada.size())
         cout << "Espaço em branco" << endl;
         for (int i = 0; i < analisador.size(); i++)
         {
-            if(isdigit(analisador[i]) == false)
+            if(isdigit(analisador[i]) == false && i==analisador.size()-1)
             {
                 cout << analisador << " é uma string" << endl;
+                if(analisador == "int")
+                {
+                    cout << analisador << " é a palavra reservada int" << endl;
+                    //Proximo texto seria uma variavel automaticamente
+                    marcador_de_variavel = 1;
+                }
             }
             else if(isdigit(analisador[i]) == true && i==analisador.size()-1)
             {
@@ -32,9 +39,18 @@ while(indice<entrada.size())
         {
             for (int i = 0; i < analisador.size(); i++)
             {
-                if(isdigit(analisador[i]) == false)
+                if(isdigit(analisador[i]) == false && i==analisador.size()-1)
                 {
                     cout << analisador << " é uma string" << endl;
+                    if(analisador == "int")
+                    {
+                        cout << analisador << " é a palavra reservada int" << endl;
+                    }
+                    else if(marcador_de_variavel==1)
+                    {
+                        cout << analisador << " é uma variavel" << endl;
+                        marcador_de_variavel=0;
+                    }
                 }
                 else if(isdigit(analisador[i]) == true && i==analisador.size()-1)
                 {
